5bf8aecf069ff7d0b396d909fae8842391ebedae
LukVik <lukaswvik@gmail.com>
2020-05-08 19:53:07 +0200

    content for help

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,39 @@ class PagesController < ApplicationController
   def propagation; end
   def add_ebook; end
   def about_us; end
+  def help; end
+  def searching; end
+
+  def download_books; end
+  def how_read; end
+  def where_order; end
+  def where_is_ebook; end
+  def about_bookcase; end
+  def complaint; end
+  def payment_methods; end
+  def personal_data_protection; end
+  def terms_and_conditions; end
+  def newsletter; end
+  def about_registration; end
+
+  def isbn; end
+  def commission; end
+  def price; end
+  def selfpublishing; end
+  def data; end
+  # def other_services; end
+  #
+  # def license; end
+  # def price; end
+  #
+  # def formts; end
+  # def add_to_eshop; end
+  # def good_sell; end
+  # def propagation1; end
+  # def creation_book; end
+  # def printbook; end
+  # def drm; end
+  # def epub; end
+  # def mobi; end
+  # def pdf; end
 end
